1180c193a84a773c5ba659c578ece65ae0db37f5
Rodrigo Wippel <rodrigow1992@gmail.com>
2023-09-14 15:28:56 -0300

    feat/setup shadcn and linter

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,45 @@
+import { Metadata } from 'next'
+import Link from 'next/link'
+import AuthForm from '~/components/AuthForm'
+
+export const metadata: Metadata = {
+  title: 'Epanel: Auth',
+  description: 'Epanel: Auth'
+}
+
+export default function AuthenticationPage() {
+  return (
+    <main className="bg-brand-beige flex h-screen w-full flex-col">
+      <div className="container m-auto flex flex-col justify-center">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Create an account
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your email below to create your account
+            </p>
+          </div>
+          <AuthForm />
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            By clicking continue, you agree to our{' '}
+            <Link
+              href="/terms"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Terms of Service
+            </Link>{' '}
+            and{' '}
+            <Link
+              href="/privacy"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+    </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import Header from '~/components/Header'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Header />
-      <span>App</span>
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   )
 }
